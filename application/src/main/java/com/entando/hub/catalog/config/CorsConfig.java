698130a26fa377a87e9a3ee5242e192ecd58da72
AkhileshPrajapatiNV <akhilesh.prajapati@newvisionsoftware.in>
2021-12-30 17:29:04 +0530

    EHUB-180: edited corsorigin config property

--- application/src/main/java/com/entando/hub/catalog/config/CorsConfig.java
+++ application/src/main/java/com/entando/hub/catalog/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.entando.hub.catalog.config;
 
-import org.dom4j.xpath.XPathPattern;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,20 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${HUB_GROUP_DETAIL_BASE_URL}")
+    @Value("${ALLOWED_ORIGIN}")
     private String allowedOrigin;
 
-//    @Bean
-//    public WebMvcConfigurer getCorsConfigurer(){
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins(allowedOrigin)
-//                        .allowedMethods("*")
-//                        .allowedHeaders("*");
-//            }
-//        };
-//    }
+    @Bean
+    public WebMvcConfigurer getCorsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
 
