f7fb8848d6976ee30cc8b9c7bcac0525498227b3
Rohit Sengar <rohit.sengar@newvisionsoftware.in>
2021-12-30 09:27:24 +0530

    EHUB-180: Added global CORS configuration.

---
+++ application/src/main/java/com/entando/hub/catalog/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.entando.hub.catalog.config;
+
+import org.dom4j.xpath.XPathPattern;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${HUB_GROUP_DETAIL_BASE_URL}")
+    private String allowedOrigin;
+
+    @Bean
+    public WebMvcConfigurer getCorsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
