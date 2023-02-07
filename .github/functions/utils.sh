df349deeb28df3307bcc68f268ce670cf61bbb14
Alessandro Dionisi <a.dionisi@entando.com>
2023-02-07 09:24:01 +0100

    Fix

---
+++ .github/functions/utils.sh
@@ -0,0 +1,46 @@
+function getTicketNumber() {
+    local str=$1
+    result=$(echo "$str" | grep -oE "^EHUB-\d*")
+    echo "$result"
+}
+
+function setBundleVersion() {
+    local versionPrefix=$1
+    version=$(ent bundle info | grep "Version:" | awk '{print $2}')
+    value=".version=\"$versionPrefix$version\""
+    jq $value entando.json > temp.json && mv temp.json entando.json
+}
+
+function setComponentVersions() {
+    local versionPrefix=$1
+
+    components=$(ent bundle list | tail -n +$((n + 3)))
+    while IFS= read -r line
+    do
+        name=$(echo $line | awk '{print $1}')
+        type=$(echo $line | awk '{print $2}')
+        version=$(echo $line | awk '{print $3}')
+        stack=$(echo $line | awk '{print $4}')
+
+        # Enter component folder
+        cd "$type"s/"$name"
+
+        ver="$versionPrefix$version"
+        echo "Setting version=$ver for component $name"
+
+        if [ "$stack" = "spring-boot" ]; then
+            mvn versions:set -DnewVersion="$ver" > /dev/null
+        fi
+
+        if [ "$stack" = "react" ]; then
+            value=".version=\"$ver\""
+            jq $value package.json > temp.json && mv temp.json package.json
+        fi
+
+        # Return back to bundle folder
+        cd ../../
+        
+    done <<< "$components"
+}
+
+"$@"
