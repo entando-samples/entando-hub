f3b8c1861396b7d415914526a8ab62f33cf564a8
Rax Canaan W. Layumas <raxkaynan@gmail.com>
2023-05-17 00:05:09 +0800

    EHUB-337 Remove unused import in ModalAddNewBundleGroup.test.js

--- application/microfrontends/eh-app/src/page/catalog/modal-add-new-bundle-group/ModalAddNewBundleGroup.test.js
+++ application/microfrontends/eh-app/src/page/catalog/modal-add-new-bundle-group/ModalAddNewBundleGroup.test.js
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen, waitFor, prettyDOM } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 
