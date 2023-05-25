bf392ab0f01fca82d06681ae41cdd4d142189559
Rax Canaan W. Layumas <raxkaynan@gmail.com>
2023-05-25 18:58:07 +0800

    EHUB-337 Refactor Http.js and update the corresponding tests

--- application/microfrontends/eh-app/src/integration/Http.test.js
+++ application/microfrontends/eh-app/src/integration/Http.test.js
@@ -9,41 +9,38 @@ const mockPayload = { name: 'Test Data' };
 
 describe('HTTP functions', () => {
   afterEach(() => {
-    axios.get.mockReset();
-    axios.post.mockReset();
-    axios.delete.mockReset();
-    axios.put.mockReset();
+    axios.request.mockReset();
   });
 
   test('getData', async () => {
-    axios.get.mockResolvedValue({ data: mockPayload });
+    axios.request.mockResolvedValue({ data: mockPayload });
 
     const result = await getData(mockUrl, mockId);
-    expect(axios.get).toHaveBeenCalledWith(`${mockUrl}${mockId}`, expect.any(Object));
+    expect(axios.request).toHaveBeenCalledWith({ method: 'get', url: `${mockUrl}${mockId}` });
     expect(result).toEqual({ data: mockPayload, isError: false });
   });
 
   test('postData', async () => {
-    axios.post.mockResolvedValue({ data: mockPayload });
+    axios.request.mockResolvedValue({ data: mockPayload });
 
     const result = await postData(mockUrl, mockPayload, mockId);
-    expect(axios.post).toHaveBeenCalledWith(`${mockUrl}${mockId}`, mockPayload, expect.any(Object));
+    expect(axios.request).toHaveBeenCalledWith({ method: 'post', url: `${mockUrl}${mockId}`, data: mockPayload });
     expect(result).toEqual({ data: mockPayload, isError: false });
   });
 
   test('deleteData', async () => {
-    axios.delete.mockResolvedValue({ data: mockPayload });
+    axios.request.mockResolvedValue({ data: mockPayload });
 
     const result = await deleteData(mockUrl, mockId);
-    expect(axios.delete).toHaveBeenCalledWith(`${mockUrl}${mockId}`, expect.any(Object));
+    expect(axios.request).toHaveBeenCalledWith({ method: 'delete', url: `${mockUrl}${mockId}` });
     expect(result).toEqual({ data: mockPayload, isError: false });
   });
 
   test('putData', async () => {
-    axios.put.mockResolvedValue({ data: mockPayload });
+    axios.request.mockResolvedValue({ data: mockPayload });
 
     const result = await putData(mockUrl, mockPayload, mockId);
-    expect(axios.put).toHaveBeenCalledWith(`${mockUrl}${mockId}`, mockPayload, expect.any(Object));
+    expect(axios.request).toHaveBeenCalledWith({ method: 'put', url: `${mockUrl}${mockId}`, data: mockPayload });
     expect(result).toEqual({ data: mockPayload, isError: false });
   });
 
@@ -53,10 +50,10 @@ describe('HTTP functions', () => {
         name: 'Error',
       }),
     };
-    axios.get.mockRejectedValue(errorResponse);
+    axios.request.mockRejectedValue(errorResponse);
   
     const result = await getData(mockUrl, mockId);
-    expect(axios.get).toHaveBeenCalledWith(`${mockUrl}${mockId}`, expect.any(Object));
+    expect(axios.request).toHaveBeenCalledWith({ method: 'get', url: `${mockUrl}${mockId}` });
     expect(result).toEqual({ data: errorResponse, isError: true });
   });  
 });
